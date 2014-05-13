7703bc2eac749157eb9e7b546a6826e5ec27389c
hraboviyvadim <hraboviyvadim@gmail.com>
2014-05-13 11:54:49 +0300

    upd

--- js/object.js
+++ js/object.js
@@ -4,25 +4,27 @@ var loader = {
   loading: function load_percent(){
     var preloader = $('.preloader');
     var percent = $('.preloader__progress');
+    var intro_text = $(".preloader__text");
     var pText = $(".preloader__counter");
     var window_width = $(window).width();
     var prNum = 0;
-    var prNum2 = 0;
+    var i = 0;
 
-    preloader.append(pText);
+    //preloader.append(pText);
 
     var animation = setInterval (function(){
       percent.css('background-color', '#ffdd00');
       prNum += (window_width/100);
       
-      prNum2 += 1;
-      pText.text(""+prNum2+" %");
+      i += 1;
+      pText.text(""+i+" %");
       
       percent.css('width', prNum);
 
       if (prNum >= window_width ) {
-        pText.text("Загрузка завершена");
+        pText.hide();
         percent.css('width', window_width);
+        intro_text.css('opacity', 1);
         clearInterval(animation);
       }
     }, 15);
